f7758b9dac34f494994b9d9a48245adc98148797
5h5ong <Call5h5ong@gmail.com>
2020-04-06 14:40:40 +0900

    Initial Commit

---
+++ index.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'no-console': 'warn',
+    'react/prop-types': 'off',
+    // interface 이름 앞의 `I` prefix의 허용과 관련된 rule
+    '@typescript-eslint/interface-name-prefix': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+  },
+};
---
+++ readme.md
@@ -0,0 +1,4 @@
+# eslint 개인 설정
+- React(Hooks)
+- Prettier
+- Typescript
